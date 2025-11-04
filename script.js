d9149beab9814da843af21be2aef6c70427cfb60
Edenalia <edenelia.contact@gmail.com>
2025-11-04 19:48:32 +0100

    Site accueil direct sans bouton

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const bouton = document.getElementById('clickMe');
-    if (bouton) {
-        bouton.addEventListener('click', function() {
-            alert('Merci d\'avoir cliqué !');
-        });
-    }
-});
